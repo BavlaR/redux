70a64855c5e2f04e36e689fb230f5c8affc44568
BavlaR <vdmmailbox1@gmail.com>
2021-02-24 17:08:11 +0200

    hw

--- lesson1/hw/src/App.jsx
+++ lesson1/hw/src/App.jsx
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
-import { userData, UserDataContext } from './context';
+import { UserDataContext } from './context';
 import Header from './Header';
 
 class App extends Component {
    state = {
-      userInfo: userData,
+      userData: {
+         alt: 'User Avatar',
+         src: 'https://avatars3.githubusercontent.com/u10001',
+         className: 'menu__avatar',
+      },
    };
 
    render() {
       return (
          <div className="page">
-            <UserDataContext.Provider value={this.state.userInfo}>
+            <UserDataContext.Provider value={this.state.userData}>
                <Header />
             </UserDataContext.Provider>
          </div>
--- lesson1/hw/src/context.js
+++ lesson1/hw/src/context.js
@@ -1,9 +1,3 @@
 import React from 'react';
 
-export const userData = {
-   alt: 'User Avatar',
-   src: 'https://avatars3.githubusercontent.com/u10001',
-   className: 'menu__avatar',
-};
-
 export const UserDataContext = React.createContext();
