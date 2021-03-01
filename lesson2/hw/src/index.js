8cb0635f6006a287b658bbd1907e4e4d77bf0c04
BavlaR <vdmmailbox1@gmail.com>
2021-03-01 16:28:09 +0200

    hw

--- lesson2/hw/src/index.js
+++ lesson2/hw/src/index.js
@@ -1,11 +1,9 @@
-import { addUser, deleteUser } from './users/actions';
+import { addUser, deleteUser } from './users.actions';
 import store from './store';
 
 store.dispatch(addUser({ id: 2, name: 'John' }));
 store.dispatch(addUser({ id: 3, name: 'Bob' }));
 store.dispatch(deleteUser(2));
-
-console.log(store.getState());
 
 store.subscribe(() => {
    console.log(store.getState());
